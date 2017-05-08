67d5b96b32a38e166cefd0031ca868b647f95124
kyrylogrygorenko <kyrylogrygorenko@gmail.com>
2017-05-08 22:15:43 +0300

    Database added, Throwing db data through controller

--- Controller/BookController.php
+++ Controller/BookController.php
@@ -5,7 +5,15 @@ use Library\Controller;
 class BookController extends Controller {
     public function indexAction($route)
     {
-        return $this->render('index.phtml',$route);
+        //DB connect,model
+        $author= 'King';
+        $books= ['book1','book2','book3'];
+        $data = [
+            'author'=>$author,
+            'books'=>$books
+        ];
+
+        return $this->render('index.phtml',$route, $data);
     }
     public function showAction()
     {
