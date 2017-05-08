aac2bcefb057b7cc35853a48237e04d54961125b
kyrylogrygorenko <kyrylogrygorenko@gmail.com>
2017-05-08 20:39:56 +0300

    Controller with static method

--- Library/Controller.php
+++ Library/Controller.php
@@ -7,30 +7,30 @@ abstract class Controller
     public static $dir;
     public static $file;
 
-//    public static function render($view,$route)
-//    {
-//        self::$dir=ucfirst($route[0]);
-//        self::$file = VIEW_DIR . self::$dir . DS . $view;
-////        if (!file_exists(self::$file)) {
-////            throw new \Exception("self::$file  not found!!");
-////        }
-//        ob_start();
-//        require self::$file;
-//        return ob_get_clean();
-//    }
-
-
-    public function render($view)
+    public static function render($view, $route)
     {
-        $dir = str_replace('Controller', '', get_class($this));
-        $file = VIEW_DIR . $dir . DS . $view;
-        //echo $file;
-        if (!file_exists($file)) {
-            throw new Exception("{$file} not found!!");
-        }
-
+        self::$dir = ucfirst($route[0]);
+        self::$file = VIEW_DIR . self::$dir . DS . $view;
+//        if (!file_exists(self::$file)) {
+//            throw new \Exception("self::$file  not found!!");
+//        }
         ob_start();
-        require $file;
+        require self::$file;
         return ob_get_clean();
     }
+
 }
+//    public function render($view)
+//    {
+//        $dir = str_replace('Controller', '', get_class($this));
+//        $file = VIEW_DIR . $dir . DS . $view;
+//        //echo $file;
+//        if (!file_exists($file)) {
+//            throw new Exception("{$file} not found!!");
+//        }
+//
+//        ob_start();
+//        require $file;
+//        return ob_get_clean();
+//    }
+//}
